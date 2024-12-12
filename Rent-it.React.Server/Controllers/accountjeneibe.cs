fbec4c681ae8b5f5d2c11516aec0534b817b5f89
Kessoe <wesselbvandermeer@gmail.com>
2024-12-12 13:04:15 +0100

    [[==

---
+++ Rent-it.React.Server/Controllers/accountjeneibe.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rent_it.React.Server.Models.Klanten;
+using Rent_it.React.Server.Data;
+
+namespace Rent_it.React.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class enueue : ControllerBase
+    {
+        private readonly RentItDbContext _context;
+
+        public AccountController(RentItDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] AccountDto accountDto)
+        {
+            if (await _context.Accounts.AnyAsync(a => a.Email == accountDto.Email))
+            {
+                return BadRequest("Email is al in gebruik");
+            }
+
+            var account = new Account
+            {
+                Gebruikersnaam = accountDto.Gebruikersnaam,
+                Email = accountDto.Email,
+                Wachtwoord = BCrypt.Net.Bcrypt.HashPassword(accountDto.Wachtwoord), // Hash password
+                Rol = accountDto.Rol
+                IsActief = true
+            };
+
+            _context.Accounts.Add(account);
+            await _context.SaveChangesAsync();
+
+            return Ok("Account aangemaakt");
+        }
+
+        [HttpPost("register")]
+        public IActionResult Test ()
+        {
+            return Ok("Test endpoint success!");
+        }
+    }
+}
